7317f62fc5937a644f1fa04952e865d270f15202
Muniharsha51 <muniharsha51@gmail.com>
2025-03-27 01:05:51 +0530

    third commit

---
+++ loops.py
@@ -0,0 +1,170 @@
+
+
+
+# # -------------------------------------------EASY LEVEL QUESTIONS----------------------------
+
+# # Q-1 :  Print all numbers from 1 to 100 using a  for  loop.
+
+# # for i in range(1,101):
+# #     print(i)
+
+
+
+#Q-2 :---------------- Write a program to print the sum of the first  n  natural numbers. 
+
+# num=50
+# sum=0
+# for i in range(1,num):
+#     sum=sum+i
+# print(sum)
+
+
+#Q-3 :------------------Print all even numbers between 1 and 50 using a  while loop. 
+
+# num=0
+# while num<50:
+#     num +=2
+#     print(num)
+
+
+
+#Q-4 :-------------------Write a program to display the multiplication table of a given number. First 20 
+
+
+# num = 5
+# mul=1
+# for i in range(1,21):
+#     mul=num*i
+#     print(mul)
+
+
+# Q-5 : Reverse a number using a  while  loop.  Also can we get the sum of all the digits. 
+# num1=5
+# sum=0
+# while num1>0:
+#     num1 -=1
+#     print(num1)
+
+# # n = float(input("enter a number: "))
+# n=5
+# i = 1
+# sum = 0
+# while (i <= n):
+#     sum = sum + i
+#     i = i + 1
+# print("The sum is: ", sum)
+
+
+
+# Q-6 : Write a program to count the number of digits in a given number using a  while  loop.
+
+# num=400
+# count=0
+# while num>0:
+#     num=num//10
+#     count+=1
+# print('count of digits in given number',count)
+
+# Q-7 : Write a program that keeps asking the user to enter numbers  until they enter a negative number. Use a  while  loop. 
+
+# while True:
+#     num = int(input("Enter a number : "))
+#     if num < 0:
+#         print("Negative number entered. Stopping the program.")
+#         break 
+#     print("You entered:", num, 'try another number')
+
+
+
+
+
+# # -------------------------------------------MEDIUM LEVEL QUESTIONS----------------------------
+
+
+# Q-1 : Print the first 10 terms of the Fibonacci series using a  for loop.
+
+# num1=0
+# num2=1
+# fib=0
+# for i in range(0,10):
+#     fib=num1+num2
+#     num1=num2
+#     num2=fib
+#     print(fib)
+
+
+
+#Q-2 : Check if a given number is a prime number using a for loop. 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------GENERAL PRATICE : 
+
+# list1 =[1,22,33,44]
+# for i in list1:
+#     print(i)
+
+
+# for i in range(0,len(list1)):
+#     print(i)
+    
+
+# for i in range(1,20):
+#     print(i)
+
+# --------EVEN NUMBERS-----------
+# for i in range(0,40):
+#     if i%2==0:
+#         print(i)
+
+# --------ODD NUMBERS--------------
+# for i in range(0,40):
+#     if i%2!=0:
+#         print(i)
+
+# sum=0
+# for i in range(1,21):
+#     sum=sum+i
+# print(sum)
+
+
+# --------------------------Factorial of the given number------
+# 5=5*4*3*2*1 = 120
+
+# num=5
+# factoial=1
+# for i in range (num,0,-1):
+#     factoial=factoial*i
+# print(factoial)
+
+
+
+#--------------------------- While loop ----------------------------
+
+# num1=5
+# while num1>0:
+#     num1 -=1
+#     print(num1)
+
+
+
+
+
+
+
+
