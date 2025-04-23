0b06325a92c2fbfa2cdeb8b4ed2ffb4dc283f654
Muniharsha51 <muniharsha51@gmail.com>
2025-04-23 16:39:22 +0530

    oops starting

---
+++ oops.py
@@ -0,0 +1,11 @@
+# class Calculator :
+#     def add(self):
+#         print("This is an add function")
+#     def mul(self):
+#         print
+
+# calc1=Calculator()
+# calc1=Calculator()
+
+# calc1=add()
+# calc1=add()
