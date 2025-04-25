6c5bd09ea00daf170d5158de79d0ccc8834ed0f5
Muniharsha51 <muniharsha51@gmail.com>
2025-04-25 16:57:08 +0530

    oops concept

--- Patterns.py
+++ Patterns.py
@@ -148,8 +148,9 @@
 # 12345
 
 # n=5
+# start=1   # if we initial the start inside in a loop in pattern in every line numbers are start with 1
 # for i in range(n+1):
-#     start=1
+    
 #     for j in range(n):
 #         if i>j :
 #             print(start, end='')
@@ -182,9 +183,13 @@
 #--------------------------------------------------------------------------------------
 # print(chr(97))
 # print(chr(122))
-
 # print(ord('A'))
 # print(ord('['))
+
+# A
+# BC
+# DEF
+# GHIJ
 
 # n=4
 # start=65
@@ -195,4 +200,145 @@
 #             start+=1
 #     print()
 
+#--------------------------------------------------------------------------------------
+
+# Q.
+#       * 
+#      * * 
+#     * * *
+#    * * * *
+#   * * * * *
+#  * * * * * *
+# * * * * * * *
+
+# n=7
+# for i in range(n):
+#     for k in range(n-i-1):
+#         print(" ", end='')
+
+#     for j in range(n):
+#         if i>=j:
+#             print('*', end=' ')
+#     print()
+
+#--------------------------------------------------------------------------------------
+
+# Q.
+#       *
+#      * *
+#     *   *
+#    *     *
+#   *       *
+#  *         *
+# * * * * * * *
+# n=7
+# for i in range(n):
+#     for k in range(n-i-1):
+#         print(" ", end='')
+
+#     for j in range(n):
+#         if i==j or i==n-1 or j==0:
+#             print('*', end=' ')
+#         else:
+#             print(" ",end=' ')
+#     print()
+
+
+
+#Q. -------------------------------------------Fibinocci---------------------------------
+
+# 0 
+# 1 1 
+# 2 3 5
+# 8 13 21 34
+# 55 89 144 233 377
+# 610 987 1597 2584 4181 6765
+# 10946 17711 28657 46368 75025 121393 196418
+
+# n=7 
+# num1, num2=0,1
+# for i in range(n):
+#     for j in range(n):
+#         if i>=j:
+#             print(num1,end=' ')
+#             num1,num2=num2,num1+num2
+#     print()
+
+#--------------------------------------------------------------------------------------
+# Q.
+#             1 
+#           2 1 0 
+#         3 2 1 0 -1
+#       4 3 2 1 0 -1 -2
+#     5 4 3 2 1 0 -1 -2 -3
+#   6 5 4 3 2 1 0 -1 -2 -3 -4
+# 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5
+
+# n=7
+# for i in range(n):
+#     start=i+1
+#     for k in range(2*(n-i-1)):
+#         print(' ', end='')
+#     for j in range(2*i+1):
+#         print(start, end=' ')
+#         start-=1
+#     print()
+
+#--------------------------------------------------------------------------------------
+
+#             1
+#           2 1 2
+#         3 2 1 2 3
+#       4 3 2 1 2 3 4
+#     5 4 3 2 1 2 3 4 5
+#   6 5 4 3 2 1 2 3 4 5 6
+# 7 6 5 4 3 2 1 2 3 4 5 6 7
+
+# n=7
+# for i in range(n):
+#     track1=False
+#     start=i+1
+#     for k in range(2*(n-i-1)):
+#         print(' ', end='')
+
+#     for j in range(2*i+1):
+#         print(start, end=' ')
+#         if start==1:
+#             track1=True
+
+#         if track1== False:
+#             start=start-1
+#         else:
+#             start+=1
+#     print()
+
+#--------------------------------------------------------------------------------------
+#METHOD-2( HALF CODE ONLY)
+# i=2
+# str1=''
+# for i in range(1,i+2):
+#     str1=str1+str(i)+' '
+
+# i=2
+# for i in range (i,i+2):
+#     str1=str(i)+' '+str1
+# print(str1)
+
+# FULL CODE FOR THE METHOD-2
+# rows = 7
+# result = ''
+
+# for i in range(1, rows + 1):
+#     str1 = ''
+    
+#     for j in range(i, 0, -1):
+#         str1 += str(j) + ' '
+    
+#     for j in range(2, i + 1):
+#         str1 += str(j) + ' '
+
+#     spaces = '  ' * (rows - i)
+#     result += spaces + str1.rstrip() + '\n'
+
+# print(result)
 
--- oops.py
+++ oops.py
@@ -1,11 +1,111 @@
-# class Calculator :
-#     def add(self):
-#         print("This is an add function")
-#     def mul(self):
-#         print
+class Calculator :
+    def __init__(self, id1, color1, manf_date1):
+        self.id=id1
+        self.color=color1
+        self.manf_date=manf_date1
+        print('this is a constructor')
+    
+    def print_id(self):
+        print(Calculator.company_name)
+        print(self.id)
 
-# calc1=Calculator()
-# calc1=Calculator()
+    def add(self):
+        print("This is an add function")
 
-# calc1=add()
-# calc1=add()
+    def mul(self):
+        print("this is a function for multiflication")
+
+    @staticmethod
+    def print_company_desc():
+        print("we don't use any variables")
+
+calc1=Calculator(1,'black','01feb')
+calc1=Calculator(2,'white','01feb')
+
+calc1.add()
+calc1.add()
+
+calc1.id=12
+calc1.color='black'
+calc1.manf_date='01feb'
+calc1.id=14
+
+#METHODS:
+
+calc1.print_id()
+
+
+Calculator.change_company_name('NEW_CASIO')
+calc1.change_company_name('NEW_CASIO10')
+print(calc1.company_name)
+
+@classmethod
+def change_company_name(cls,new_name):
+    cls.company_name = new_name
+    print("company name changed")
+
+#EXPLANATION OF ABOVE CODE -----------------------------------------------------------------------------
+# Defining the Calculator class
+class Calculator:
+
+    # Class Attribute - shared by all instances of the class
+    company_name = "CASIO"
+
+    # Constructor - automatically called when an object is created
+    def __init__(self, id1, color1, manf_date1):
+        self.id = id1                     # Instance Attribute
+        self.color = color1              # Instance Attribute
+        self.manf_date = manf_date1      # Instance Attribute
+        print('This is a constructor')   # Just a message
+
+    # Instance Method - works with object-specific data
+    def print_id(self):
+        print(Calculator.company_name)   # Access class attribute using class name
+        print(self.id)                   # Access instance attribute
+
+    def add(self):
+        print("This is an add function")  # Simulated addition method
+
+    def mul(self):
+        print("This is a function for multiplication")  # Simulated multiplication method
+
+    # Static Method - doesn't use self or cls; general utility
+    @staticmethod
+    def print_company_desc():
+        print("Static Method: We don't use any object/class variables here.")
+
+    # Class Method - modifies class attribute
+    @classmethod
+    def change_company_name(cls, new_name):
+        cls.company_name = new_name
+        print("Class Method: Company name changed to", cls.company_name)
+
+
+# Creating an object of Calculator
+calc1 = Calculator(1, 'black', '01-Feb')  # Constructor is called
+
+# Overwriting with a new object (only this will be used)
+calc1 = Calculator(2, 'white', '01-Feb')  # Constructor is called again
+
+# Calling instance methods
+calc1.add()         # Outputs add function message
+calc1.mul()         # Outputs multiplication function message
+
+# Modifying instance attributes
+calc1.id = 14
+calc1.color = 'black'
+calc1.manf_date = '01-Feb'
+
+# Accessing instance method that prints instance and class data
+calc1.print_id()
+
+# Calling class method from both class and object
+Calculator.change_company_name('NEW_CASIO')     # Using class
+calc1.change_company_name('NEW_CASIO10')        # Using object
+
+# Printing updated class attribute
+print("Company Name:", calc1.company_name)
+
+# Calling static method
+Calculator.print_company_desc()
+#---------------------------------------------------------------------------------------------------------------
